f0082b51e988417706893f817f613bb1562b4437
kadirpekel <kadirpekel@gmail.com>
2013-01-07 21:53:34 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='liquer',
     py_modules=['liquer'],
-    version='0.0.2',
+    version='0.0.3',
     description='Query your objects for two cents!',
     author='Kadir Pekel',
     author_email='kadirpekel@gmail.com',
